5a0f2d422faf30ac9360300d9db85304b567d4dd
Frances Berriman <fberriman@gmail.com>
2014-04-08 15:20:55 -0700

    Add stubbed out kitchen dashboard WIP

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <!--link rel="stylesheet" href="http://style.codeforamerica.org/0/style/css/main.css">
+        <link rel="stylesheet" href="http://style.codeforamerica.org/0/style/css/layout.css" media="all and (min-width: 40em)">
+        <link href="http://style.codeforamerica.org/0/style/css/prism.css" rel="stylesheet" /-->
+        <link href="style/main.css" rel="stylesheet" />
+        <title>CFA Dashboard</title>
+    </head>    
+    <body>
+        <h1>Code for America Dashboard <span>Last updated on 4th April 2014 at 14:45</h1>
+        <!--nav class="nav-tabs" role="navigation">
+            <ul class="layout-semibreve layout-tight">
+                <li class="nav-tab"><a class="nav-tab-link" href="edit.html">English</a></li>
+                <li class="nav-tab"><a class="nav-tab-link" href="#">Spanish</a></li>
+                <li class="nav-tab"><a class="nav-tab-link" href="#">Chinese</a></li>
+            </ul>
+        </nav-->
+        <!--ul class="detailed-info">
+            <li><a href="">Stats</a></li>
+            <li><a href="history.html">History</a></li>
+        </ul-->
+
+        <div class="layout-breve">
+    
+            <div class="bigstats fruit">
+                <h2>Food</h2>
+                <ul>
+                    <li><span class="value">564</span> bananas eaten</li>
+                    <li><span class="value">23</span> pounds of Hot Tomales chomped</li>
+                    <li><span class="value">15</span> oranges squeezed</li>
+                </ul>
+            </div>
+            
+            <div class="bigstats beverages">
+                <h2>Beverages</h2>
+                <ul>
+                    <li><span class="value">1587</span> cups of coffee made</li>
+                    <li><span class="value">13</span> gallons of milk consumed</li>
+                    <li><span class="value">157</span> cups of decaf made</li>
+                </ul>
+            </div>
+
+            <div class="bigstats office">
+                <h2>Office</h2>
+                <ul>
+                    <li><span class="value">12</span> InstaCart deliveries</li>
+                    <li><span class="value">13</span> gallons of milk consumed</li>
+                    <li><span class="value">157</span> cups of decaf made</li>
+                </ul>
+            </div>
+           <div class="bigstats fruit">
+                <h2>Food</h2>
+                <ul>
+                    <li><span class="value">564</span> bananas eaten</li>
+                    <li><span class="value">23</span> pounds of Hot Tomales chomped</li>
+                    <li><span class="value">15</span> oranges squeezed</li>
+                </ul>
+            </div>
+            
+            <div class="bigstats beverages">
+                <h2>Beverages</h2>
+                <ul>
+                    <li><span class="value">1587</span> cups of coffee made</li>
+                    <li><span class="value">13</span> gallons of milk consumed</li>
+                    <li><span class="value">157</span> cups of decaf made</li>
+                </ul>
+            </div>
+
+            <div class="bigstats office">
+                <h2>Office</h2>
+                <ul>
+                    <li><span class="value">12</span> InstaCart deliveries</li>
+                    <li><span class="value">13</span> gallons of milk consumed</li>
+                    <li><span class="value">157</span> cups of decaf made</li>
+                </ul>
+            </div>
+           
+            <div class="warzone">
+                <h2>Days kitchen has been declared a disaster zone:</h2>
+                <p>14</p>
+            </div>
+
+
+
+        </div>
+
+     
+        
+        
+
+    
+    </body>
+</html>
