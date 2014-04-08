5a4278b6c7c81c081c0fc64866f27fee89ee07fc
Frances Berriman <fberriman@gmail.com>
2014-04-08 15:51:22 -0700

    Remove default table styles

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
             <li><a href="history.html">History</a></li>
         </ul-->
 
-        <table width="100%">
+        <table width="100%" cellpadding=0 cellspacing=0 border=0>
         <tr>
             <td class="bigstats fruit">
                 <h2>Food</h2>
